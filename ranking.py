b51261981fb182efcb2e708c2296dd4cb67b89f6
re1nth <re1nth98@gmail.com>
2019-04-10 18:27:12 +0530

    Optimized ranking

--- ranking.py
+++ ranking.py
@@ -13,7 +13,6 @@ def group_average_rating(review_group_matrix, volumes, ratings):
 
 def get_group_rankings(review_group_matrix, ratings,group_weights):
 	volumes = group_volume(review_group_matrix)
-	print(volumes)
 	average_ratings = group_average_rating(review_group_matrix, volumes, ratings)
 	group_count = volumes.shape[0]
 	group_scores = [ (group_weights[0] * volumes[idx] + group_weights[1]*average_ratings[idx],idx+1) for idx in range(group_count)]
@@ -36,121 +35,104 @@ def get_instance_weights(instances):
 	return np.full(instances, 1/instances)
 
 def jaccard_sim(instance1, instance2):
-    count_intersect = 0
-    count_1 = 0
-    count_2 = 0
-    for i in range(0,len(instance1)):
-        if instance1[i] == 1:
-            count_1 += 1
-        if instance2[i] == 1:
-            count_2 += 1
-        count_intersect += int((instance1[i] + instance2[i])/2)
+	count_intersect = 0
+	count_1 = len(instance1)
+	count_2 = len(instance2)
+	i = 0 
+	j = 0 
+	while(i<count_1 and j <count_2):
+		if instance1[i]==instance2[j]:
+			count_intersect = count_intersect+1
+			i = i+1
+			j = j+1
+		elif instance1[i]>instance2[j]:
+			j = j+1
+		else :
+			i = i+1
+	return float(count_intersect/(count_1 + count_2 - count_intersect))
 
-    #print(float(count_intersect/(count_1 + count_2 - count_intersect)))
-    return float(count_intersect/(count_1 + count_2 - count_intersect))
+def instance_ranking(useful_data, review_group_matrix, group_number):
 
-def group_ranking(useful_data, review_group_matrix, group_number):
-    print("Hello")
-    is_duplicate = [0]*len(useful_data)
-    unique_useful_data = []
-    unique_data_count = []
-    unique_data_proportion = []
+	is_duplicate = [0 for i in range(len(useful_data))]
+	unique_useful_data = []
+	unique_data_count = []
+	unique_data_proportion = []
 
-    print("length: ",len(useful_data))
+	attr_list=[]
 
-    similarity_cutoff = 0.8
-    for i in range(0,len(useful_data)):
-        if is_duplicate[i] != 0:
-            continue
-        data_count = 1
-        tmp = useful_data[i]
-        rating = get_rating(useful_data[i])
-        prop = review_group_matrix[i][group_number]
-        for j in range(0,len(useful_data)):
-            #print("J: ",j)
-            #print("GN: ",group_number)
+	for i in range(len(useful_data)):
+		tmp = useful_data[i]
+		attr_temp=[]
+		for j in range(len(tmp)):
+			if tmp[j]: attr_temp.append(j)
+		attr_list.append(attr_temp)
 
-            if jaccard_sim(useful_data[i],useful_data[j]) >= similarity_cutoff:
-                is_duplicate[j] = 1
-                data_count += 1
-                prop = max(prop, review_group_matrix[j][group_number])
+	similarity_cutoff = 1
 
-                if get_rating(useful_data[j]) < rating:
-                    for r in range(6):
-                        tmp[r] = 0
-                    tmp[get_rating(useful_data[j])] = 1
+	# print("Len: ",len(useful_data))
+	attr_tuple = [tuple(x) for x in attr_list]
+	# print("Set Len", len(set(attr_tuple)))
 
-        unique_data_proportion.append(prop)
-        unique_useful_data.append(tmp)
-        unique_data_count.append(data_count)
+	for i in range(0,len(attr_list)):
 
-    # Give Rating
+		if is_duplicate[i] != 0:
+			continue
+	
+		data_count = 1
+		tmp = useful_data[i]
 
+		rating = get_rating(tmp)
+		prop = review_group_matrix[i][group_number]
 
+		for j in range(i,len(attr_list)):
+			if jaccard_sim(attr_list[i],attr_list[j]) >= similarity_cutoff:
+				is_duplicate[j] = 1
+				data_count += 1
+				prop = max(prop, review_group_matrix[j][group_number])
+
+				if get_rating(useful_data[j]) < rating:
+					for r in range(6):
+						tmp[r] = 0
+					tmp[get_rating(useful_data[j])] = 1
+
+		unique_data_proportion.append(prop)
+		unique_useful_data.append(tmp)
+		unique_data_count.append(data_count)
 
 def main():
 
-    training_data_list = ["datasets/swiftkey/trainL/info.txt","datasets/swiftkey/trainL/non-info.txt"]
-    training_data_info = "datasets/swiftkey/trainL/info.txt"
-    training_data_noninfo = "datasets/swiftkey/trainL/non-info.txt"
-    test_data = "datasets/swiftkey/trainU/unlabeled.txt"
+	training_data_list = ["datasets/swiftkey/trainL/info.txt","datasets/swiftkey/trainL/non-info.txt"]
+	training_data_info = "datasets/swiftkey/trainL/info.txt"
+	training_data_noninfo = "datasets/swiftkey/trainL/non-info.txt"
+	test_data = "datasets/swiftkey/trainU/unlabeled.txt"
 
-    review_group_matrix, useful_data, mapping = model_topics(training_data_list,
-    														 training_data_info,
-    								 						 training_data_noninfo,
-    														 test_data)
+	review_group_matrix, useful_data, mapping = model_topics(training_data_list,
+															 training_data_info,
+															 training_data_noninfo,
+															 test_data)
 
-    ratings = review_ratings (useful_data)
+	ratings = review_ratings (useful_data)
 
-    instance_weights = get_instance_weights(review_group_matrix.shape[0])
-    group_weights = get_group_weights(2)
-    group_rankings = get_group_rankings(review_group_matrix, ratings, group_weights)
-    print(group_rankings)
+	instance_weights = get_instance_weights(review_group_matrix.shape[0])
+	group_weights = get_group_weights(2)
+	group_rankings = get_group_rankings(review_group_matrix, ratings, group_weights)
+	print(group_rankings)
 
-    num_of_groups = len(review_group_matrix[0])
+	num_of_groups = len(review_group_matrix[0])
 
-    groups = [[] for i in range(num_of_groups)]
+	groups = [[] for i in range(num_of_groups)]
 
-    group_cutoff = 0.01
+	group_cutoff = 0.01
 
-    for i in range(0,len(useful_data)):
-        j = 0
-        for j in range(0,num_of_groups):
-            if review_group_matrix[i][j] >= group_cutoff:
-                groups[j].append(useful_data[i])
+	for i in range(0,len(useful_data)):
+		j = 0
+		for j in range(0,num_of_groups):
+			if review_group_matrix[i][j] >= group_cutoff:
+				groups[j].append(useful_data[i])
 
-    group_ranking_result = []
-    for i in range(len(groups)-9):
-        group_ranking_result.append(group_ranking(groups[i], review_group_matrix, i))
-
-    # is_duplicate = [0]*len(useful_data)
-    # unique_useful_data = []
-    # unique_data_count = []
-    #
-    # similarity_cutoff = 0.8
-    # for i in range(0,len(useful_data)):
-    #     if is_duplicate[i] != 0:
-    #         continue
-    #     data_count = 1
-    #     tmp = useful_data[i]
-    #     rating = get_rating(useful_data[i])
-    #     for j in range(0,len(useful_data)):
-    #
-    #
-    #         if jaccard_sim(useful_data[i],useful_data[j]) >= similarity_cutoff:
-    #             is_duplicate[j] = 1
-    #             data_count += 1
-    #             if get_rating(useful_data[j]) < rating:
-    #                 for r in range(6):
-    #                     tmp[r] = 0
-    #                 tmp[get_rating(useful_data[j])] = 1
-    #
-    #             # Set Probability
-    #     unique_useful_data.append(tmp)
-    #     unique_data_count.append(data_count)
-    #
-    # print(len(unique_useful_data))
-
+	group_ranking_result = []
+	for i in range(len(groups)):
+		group_ranking_result.append(instance_ranking(groups[i], review_group_matrix, i))
 
 
 if __name__ == '__main__':
