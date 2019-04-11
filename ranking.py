e2833dd9171d2bed31998e7b165b96e3bb71d1bf
Krishna Chaitanya <chaitu7261998@gmail.com>
2019-04-11 19:50:43 +0530

    Fixed instance_ranking

--- ranking.py
+++ ranking.py
@@ -139,20 +139,23 @@ def instance_ranking(useful_data, review_group_matrix, group_number,rank_number,
 
     return (unique_useful_data, ranked_useful_data)
 
-def create_groups(useful_data, review_group_matrix):
+def create_groups(useful_data, review_group_matrix, pred_prob):
     num_of_groups = len(review_group_matrix[0])
 
     groups = [[] for i in range(num_of_groups)]
+    review_group_matrix_groups = [[] for i in range(num_of_groups)]
 
     group_cutoff = 0.01
 
     for i in range(0,len(useful_data)):
-    	j = 0
-    	for j in range(0,num_of_groups):
-    		if review_group_matrix[i][j] >= group_cutoff:
-    			groups[j].append(useful_data[i])
+        j = 0
+        for j in range(0,num_of_groups):
+            if review_group_matrix[i][j] >= group_cutoff:
+                groups[j].append(useful_data[i])
+                review_group_matrix_groups[j].append(review_group_matrix[i])
 
-    return groups
+
+    return groups, review_group_matrix_groups
 
 
 def main(app_name):
@@ -164,7 +167,7 @@ def main(app_name):
     training_data_noninfo = "datasets/" + app_name + "/trainL/non-info.txt"
     test_data = "datasets/" + app_name + "/trainU/unlabeled.txt"
 
-    review_group_matrix, useful_data, mapping = model_topics(training_data_list,
+    review_group_matrix, useful_data, mapping, pred_prob = model_topics(training_data_list,
     														 training_data_info,
     														 training_data_noninfo,
     														 test_data)
@@ -176,13 +179,13 @@ def main(app_name):
     group_weights = get_group_weights(2)
     group_rankings, group_scores = get_group_rankings(review_group_matrix, ratings, group_weights)
 
-    groups = create_groups(useful_data, review_group_matrix)
+    groups, review_group_matrix_groups = create_groups(useful_data, review_group_matrix, pred_prob)
 
     group_ranking_result = []
     instance_ranking_result = []
     rank =1
     for idx in range(len(group_rankings)):
-        group_rank, instance_rank = instance_ranking(groups[idx], review_group_matrix, idx, rank,reverse_mapping)
+        group_rank, instance_rank = instance_ranking(groups[idx], review_group_matrix_groups[idx], idx, rank,reverse_mapping)
         group_ranking_result.append(group_rank)
         instance_ranking_result.append(instance_rank)
         rank = rank + 1
